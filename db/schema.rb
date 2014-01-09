85e9b95e719a6ab10cd0bfa97bea894b67a9295e
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 13:54:23 -0500

    Index on user's email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109151652) do
+ActiveRecord::Schema.define(version: 20140109185326) do
 
   create_table "applicants", force: true do |t|
     t.string   "name"
@@ -76,5 +76,7 @@ ActiveRecord::Schema.define(version: 20140109151652) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
