99dd6f5d72f8b604a5a4d84b73633345871f2271
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 14:38:14 -0500

    Update rake tasks with default user

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,11 +1,13 @@
 namespace :db do
   desc "Generate some sample data"
   task populate: :environment do
+    User.delete_all
     make_users
   end
 end
 
 def make_users
+  FactoryGirl.create(:test)
   FactoryGirl.create(:admin)
   FactoryGirl.create(:moderator)
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,27 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  factory :test, class: User do
+    name 'Test User'
+    email 'test@example.com'
+    password 'abcdef'
+    password_confirmation 'abcdef'
+    level 0
+  end
+
   factory :admin, class: User do
     email    { Faker::Internet.email }
     name     { Faker::Name.name }
-    password 'foobarbaz'
-    password_confirmation 'foobarbaz'
+    password 'abcdef'
+    password_confirmation 'abcdef'
     level 0
   end
 
   factory :moderator, class: User do
     email    { Faker::Internet.email }
     name     { Faker::Name.name }
-    password 'foobarbaz'
-    password_confirmation 'foobarbaz'
+    password 'abcdef'
+    password_confirmation 'abcdef'
     level 2
   end
 end
