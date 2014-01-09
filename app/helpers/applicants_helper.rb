cbd544f56bb4fe39de3629774f8da327f0ecd477
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 16:03:31 -0500

    Add helper methods for identifiers

--- app/helpers/applicants_helper.rb
+++ app/helpers/applicants_helper.rb
@@ -1,2 +1,8 @@
 module ApplicantsHelper
+  def new_applicant_identifier
+    begin
+      identifier = SecureRandom.urlsafe_base64(36)
+    end while Applicant.find_by_identifier(identifier)
+    identifier
+  end
 end
