5bc54e24ad75e01287715d771a6a9a891e8e96fa
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 11:46:13 -0500

    Sessions sign in functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  # Send to sign in page
+  def new
+  end
+
+  # Generate token, set it on the user, set the cookie
+  def create
+    user = User.find_by_email(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      sign_in user
+      # TODO: redirect somewhere
+    else
+      flash.now[:error] = 'Invalid email/password'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
