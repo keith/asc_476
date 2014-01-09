62b7f4fa674f88dac7b81b5c6f52c6e263d9e3f0
John Hiott <jhiott@oolcay.com>
2014-01-09 14:19:15 -0500

    login method fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109151652) do
+ActiveRecord::Schema.define(version: 20140109191257) do
 
   create_table "applicants", force: true do |t|
     t.string   "name"
@@ -70,11 +70,11 @@ ActiveRecord::Schema.define(version: 20140109151652) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.integer  "level"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
 
 end
