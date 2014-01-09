8e29a1b96d220b4eb953c285e8966a849a903100
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 14:22:30 -0500

    Setup rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @admin = FactoryGirl.create(:admin)
+  end
+
+  subject { @admin }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:level) }
+  it { should be_valid }
+
+  describe "user with invalid fields" do
+    before { @invalid = User.new }
+    specify { @invalid.should_not be_valid }
+  end
 end
