b8cc027b0698aafdbe022e56dd2f1ee2af76c657
John Hiott <jhiott@oolcay.com>
2014-01-09 16:27:31 -0500

    protected course routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :signed_in_user
 
   # GET /courses
   def index
