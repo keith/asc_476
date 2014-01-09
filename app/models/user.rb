510e22bc6bb6175e474bc12ec5066555b656be7f
Keith Smiley <keithbsmiley@gmail.com>
2014-01-09 11:36:52 -0500

    Add basic user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def delete_remember_token
+    self.update_attribute(:remember_token, nil)
+  end
+
+  def admin?
+    self.level == 0
+  end
+
+  def moderator?
+    self.level <= 2
+  end
 end
