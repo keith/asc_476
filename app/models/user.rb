e6874bcc020394aeb08108b32647ccf61a2f4e07
John Hiott <jhiott@oolcay.com>
2014-01-09 13:47:09 -0500

    added login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
 
+  has_secure_password
+  
   def self.new_remember_token
     SecureRandom.urlsafe_base64
   end
