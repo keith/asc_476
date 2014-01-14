90eef84f2570f164319926ad3cf639c6b65b27ec
Keith Smiley <keithbsmiley@gmail.com>
2014-01-14 18:54:08 -0500

    Working on new courses view spec

--- spec/views/courses/new.html.erb_spec.rb
+++ spec/views/courses/new.html.erb_spec.rb
@@ -1,20 +1,33 @@
 require 'spec_helper'
 
-describe "courses/new" do
-  before(:each) do
-    assign(:course, stub_model(Course,
-      :designator => "MyString",
-      :number => 1
-    ).as_new_record)
+# describe "courses/new" do
+#   before(:each) do
+#     assign(:course, stub_model(Course,
+#       :designator => "MyString",
+#       :number => 1
+#     ).as_new_record)
+#   end
+
+#   it "renders new course form" do
+#     render
+
+#     # Run the generator again with the --webrat flag if you want to use webrat matchers
+#     assert_select "form[action=?][method=?]", courses_path, "post" do
+#       assert_select "input#course_designator[name=?]", "course[designator]"
+#       assert_select "input#course_number[name=?]", "course[number]"
+#     end
+#   end
+# end
+
+describe '/courses/new', type: :feature do
+  before do
+    admin = FactoryGirl.create(:admin)
+    sign_in(admin)
   end
 
-  it "renders new course form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", courses_path, "post" do
-      assert_select "input#course_designator[name=?]", "course[designator]"
-      assert_select "input#course_number[name=?]", "course[number]"
-    end
+  it 'creates a new course' do
+    visit '/courses/new'
+    expect(page).not_to have_content('Login')
   end
 end
+
