6aa61ff56481e862202841c0de7e6a5ef335cc4a
Keith Smiley <keithbsmiley@gmail.com>
2014-01-14 16:36:24 -0500

    Write tests for helpers with methods

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,15 +1,36 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionsHelper. For example:
-#
-# describe SessionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe SessionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.create(:admin) }
+
+  describe 'sign_in' do
+    it 'should update the user with a token' do
+      expect(@user.remember_token).to be_nil
+      helper.sign_in(@user)
+      expect(@user.remember_token).not_to be_nil
+    end
+
+    it 'should set current_user' do
+      expect(helper.current_user).to be_nil
+      helper.sign_in(@user)
+      expect(helper.current_user).to eq(@user)
+    end
+  end
+
+  describe 'sign_out' do
+    before { helper.sign_in(@user) }
+
+    it 'should delete the remember_token' do
+      expect(@user.remember_token).not_to be_nil
+      helper.sign_out
+      expect(@user.remember_token).to be_nil
+    end
+
+    it 'should nil the current_user' do
+      expect(helper.current_user).not_to be_nil
+      helper.sign_out
+      expect(helper.current_user).to be_nil
+    end
+  end
 end
+
