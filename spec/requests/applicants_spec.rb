dcdb8814f8b9bdf29e0d5000e12d35e4950835c6
Grandy Nguyen <grandy.nguyen@gmail.com>
2014-02-05 10:53:22 -0500

    basic tests for /applicants

--- spec/requests/applicants_spec.rb
+++ spec/requests/applicants_spec.rb
@@ -9,14 +9,31 @@ describe "Applicants" do
     click_button 'Sign in'
   end
 
-  describe "GET /applicant/:id" do
+  describe "GET applicant_path(@applicant)" do
     before do
       @applicant = FactoryGirl.create(:applicant)
       visit applicant_path(@applicant)
     end
 
-    it "should have the url /applicant/:hash" do
+    it "should be at url /applicant/:hash" do
       current_path.should == "#{applicants_path}/#{@applicant.identifier}"
+    end
+
+  end
+
+  describe "GET applicant_path(@applicant)" do
+    before do
+      visit applicants_path
+    end
+
+    it 'should have the correct HTML' do
+      expect(page).to have_content('Listing applicants')
+    end
+
+    it 'should list all applicants' do
+      Applicant.all.each do |applicant|
+        expect(page).to have_content(applicant.name)
+      end
     end
   end
 end
