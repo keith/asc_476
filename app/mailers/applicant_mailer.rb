1eb6bf28debc32fabcedb05344ccd82ba54245ab
Keith Smiley <keithbsmiley@gmail.com>
2014-02-20 13:26:08 -0500

    Working on applicant mailer

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -1,7 +1,10 @@
 class ApplicantMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def account_email(email)
-    mail to: email, subject: 'Your ASC tutoring application'
+  def account_email(user)
+    @name = user.name
+    # TODO: Make sure this actually resolves the correct URL in production
+    @link = applicant_url(user)
+    mail to: user.email, subject: 'Your ASC tutoring application'
   end
 end
