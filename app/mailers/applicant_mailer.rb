d69ca7a3e2c83a6aa19250f6dde934b362893134
Keith Smiley <keithbsmiley@gmail.com>
2014-02-20 13:41:58 -0500

    Mailer default host options

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -3,7 +3,6 @@ class ApplicantMailer < ActionMailer::Base
 
   def account_email(user)
     @name = user.name
-    # TODO: Make sure this actually resolves the correct URL in production
     @link = applicant_url(user)
     mail to: user.email, subject: 'Your ASC tutoring application'
   end
