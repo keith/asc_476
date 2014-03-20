b4b0931a2a98eb58d9572134f69bfe1448ea2249
Keith Smiley <keithbsmiley@gmail.com>
2014-03-20 13:18:08 -0400

    All users wording

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -4,18 +4,18 @@ describe 'users/index' do
   before(:each) do
     assign(:users, [
       stub_model(User,
-        :name => 'Name',
-        :email => 'Email',
-        :password => 'Password',
-        :password_confirmation => 'Password Confirmation',
-        :level => 1
+        name: 'Name',
+        email: 'Email',
+        password: 'Password',
+        password_confirmation: 'Password Confirmation',
+        level: 1
       ),
       stub_model(User,
-        :name => 'Name',
-        :email => 'Email',
-        :password => 'Password',
-        :password_confirmation => 'Password Confirmation',
-        :level => 1
+        name: 'Name',
+        email: 'Email',
+        password: 'Password',
+        password_confirmation: 'Password Confirmation',
+        level: 1
       )
     ])
   end
@@ -23,10 +23,10 @@ describe 'users/index' do
   it 'renders a list of users' do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select 'tr>td', :text => 'Name'.to_s, :count => 2
-    assert_select 'tr>td', :text => 'Email'.to_s, :count => 2
-    assert_select 'tr>td', :text => 'Password'.to_s, :count => 0
-    assert_select 'tr>td', :text => 'Password Confirmation'.to_s, :count => 0
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
+    assert_select 'tr>td', text: 'Email'.to_s, count: 2
+    assert_select 'tr>td', text: 'Password'.to_s, count: 0
+    assert_select 'tr>td', text: 'Password Confirmation'.to_s, count: 0
   end
 end
 
