53b87a85393966ad8ba2cf428ee10976eb358626
Keith Smiley <keithbsmiley@gmail.com>
2014-03-20 13:12:19 -0400

    Date format without text. Show email suffix on pages

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
 EMAIL_SUFFIX = '@winthrop.edu'
-DATE_FORMAT  = 'Last Updated %e-%-m-%Y'
+DATE_FORMAT = '%-m-%e-%Y'
+DATE_FORMAT_TEXT  = "Last Updated #{ DATE_FORMAT }"
 AVAILABLE_TIME_FORMAT = '%l:%M %p'
