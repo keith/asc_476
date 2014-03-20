0c26243b2a3201732a108bd6141bd84c0ac2a42a
Keith Smiley <keithbsmiley@gmail.com>
2014-03-20 15:08:17 -0400

    Remove old route link

--- spec/views/positions/index.html.erb_spec.rb
+++ spec/views/positions/index.html.erb_spec.rb
@@ -1,33 +1,30 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "positions/index" do
-#   before(:each) do
-#     assign(:positions, [
-#       stub_model(Position,
-#         :professor_comments => "MyText",
-#         :professor_verdict => false,
-#         :professor_taught => false,
-#         :application_status => 1,
-#         :asc_comments => "MyText"
-#       ),
-#       stub_model(Position,
-#         :professor_comments => "MyText",
-#         :professor_verdict => false,
-#         :professor_taught => false,
-#         :application_status => 1,
-#         :asc_comments => "MyText"
-#       )
-#     ])
-#   end
+describe 'positions/index' do
+  before(:each) do
+    assign(:positions, [
+      stub_model(Position,
+        professor_comments: 'MyText',
+        professor_verdict: false,
+        professor_taught: false,
+        application_status: 1,
+        asc_comments: 'ASCText'
+      ),
+      stub_model(Position,
+        professor_comments: 'MyText',
+        professor_verdict: false,
+        professor_taught: false,
+        application_status: 1,
+        asc_comments: 'ASCText'
+      )
+    ])
+  end
 
-#   it "renders a list of positions" do
-#     render
-#     # Run the generator again with the --webrat flag if you want to use webrat matchers
-#     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-#     assert_select "tr>td", :text => false.to_s, :count => 2
-#     assert_select "tr>td", :text => false.to_s, :count => 2
-#     assert_select "tr>td", :text => 1.to_s, :count => 2
-#     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-#   end
-# end
-
+  it 'renders a list of positions' do
+    render
+    assert_select 'tr>td', text: 'MyText', count: 2
+    assert_select 'tr>td', text: 'No', count: 4
+    assert_select 'tr>td', text: Status.for_number(1).to_s, count: 2
+    assert_select 'tr>td', text: 'ASCText', count: 2
+  end
+end
