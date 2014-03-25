05725fbbcd76934b56c6076532b57882359cc34c
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 14:09:46 -0400

    Positions default applicaiton_status

--- spec/factories/positions.rb
+++ spec/factories/positions.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     professor_comments Faker::Lorem.paragraph
     professor_verdict { [true, false].sample }
     professor_taught { [true, false].sample }
-    application_status { rand(Status.names.count).to_s }
     asc_comments Faker::Lorem.paragraph
     applicant
     professor
