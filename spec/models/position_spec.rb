712af4851b61302331f4859db9893c8240dc6807
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 13:51:59 -0400

    Positions factory and identifier

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Position do
-end
+  before { @position = FactoryGirl.create(:position) }
+  subject { @position }
 
+  it { should respond_to(:applicant) }
+  it { should respond_to(:course) }
+  it { should respond_to(:professor) }
+  it { should respond_to(:identifier) }
+
+  describe 'identifier is immutable' do
+      specify { expect{ @position.update_attributes!(identifier: 123) }.to raise_exception(/can't be changed/) }
+  end
+end
