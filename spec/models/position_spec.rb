f9f0a6bde61b8ce26371d805ec1038417502d3ac
Keith Smiley <keithbsmiley@gmail.com>
2014-03-25 14:06:01 -0400

    Validate position application_status range

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -8,8 +8,26 @@ describe Position do
   it { should respond_to(:course) }
   it { should respond_to(:professor) }
   it { should respond_to(:identifier) }
+  it { should be_valid }
 
   describe 'identifier is immutable' do
       specify { expect{ @position.update_attributes!(identifier: 123) }.to raise_exception(/can't be changed/) }
+  end
+
+  describe 'validates application status' do
+    describe 'too low' do
+      before { @position.application_status = -1 }
+      it { should_not be_valid }
+    end
+
+    describe 'too high' do
+      before { @position.application_status = Status.names.count + 100 }
+      it { should_not be_valid }
+    end
+
+    describe 'in range' do
+      before { @position.application_status = Status.names.count / 2 }
+      it { should be_valid }
+    end
   end
 end
