5f82b42020707ca62dcf14acf1bf282ac3ef5245
Keith Smiley <keithbsmiley@gmail.com>
2014-04-15 14:31:25 -0400

    Dynamically build email

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -1,13 +1,25 @@
 class ApplicantMailer < ActionMailer::Base
   # TODO: Fix these email from addresses
-  default from: 'from@example.com',
-      reply_to: 'foo@example.com'
+  default from: 'from@example.com'
+  #     reply_to: 'foo@example.com'
 
   def account_email(user)
-    @count = user.positions.count
+    email_identifier = self.class.name + ':' + __method__.to_s
+    email_template = Email.find_by_identifier(email_identifier)
+    return unless email_template
+
+    # @count = user.positions.count
     @name = user.name
-    @link = applicant_url(user)
+    @url = applicant_url(user)
+
+    body = email_template.body.dup
+    body.gsub!(/\[NAME\]/i, @name)
+    body.gsub!(/\[URL\]/i, @url)
     email = user.email + EMAIL_SUFFIX
-    mail to: email, subject: 'Your ASC tutoring application'
+    mail(to: email,
+         body: body,
+         content_type: 'text/plain',
+         subject: email_template.subject,
+         reply_to: email_template.reply_to)
   end
 end
