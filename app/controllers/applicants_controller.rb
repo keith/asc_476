35123aca3bafb86a822d65a2e16dc27f6d32b65d
John Hiott <jhiott@oolcay.com>
2014-04-17 12:13:31 -0400

    fixed lack of blank position on new application

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -14,6 +14,9 @@ class ApplicantsController < ApplicationController
   # GET /applicants/new
   def new
     @applicant = Applicant.new
+    professor = Professor.new
+    position = @applicant.positions.build
+    position.professor = professor
   end
 
   # GET /applicants/1/edit
