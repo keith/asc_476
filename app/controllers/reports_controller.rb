100fc7d20d6cebe197ceffaee79c083ea90b56c4
Keith Smiley <keithbsmiley@gmail.com>
2014-04-17 11:48:24 -0400

    Allow sortable applicant name reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,18 @@
 class ReportsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   # GET /reports
   def index
-    @reports = Report.all
+    @applicants = Applicant.order(sort_column + ' ' + sort_direction)
   end
+
+  private
+
+    def sort_column
+      Applicant.column_names.include?(params[:sort]) ? params[:sort] : 'name'
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+    end
 end
