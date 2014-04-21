d76d0f383552c6973f6e9d00792219a8dd58ec30
Keith Smiley <keithbsmiley@gmail.com>
2014-04-21 16:20:36 -0400

    Factory application status

--- spec/factories/positions.rb
+++ spec/factories/positions.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     professor_comments Faker::Lorem.paragraph
     professor_verdict { [true, false].sample }
     professor_taught { [true, false].sample }
+    application_status { (0...Status.statuses.count).to_a.sample }
     applicant
     professor
     course
