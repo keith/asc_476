d3a9e2e7c0e21c482876b5cb7ee978b9dbd6baff
Keith Smiley <keithbsmiley@gmail.com>
2014-04-22 09:41:01 -0400

    Reports ignore blank courses

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,7 @@ class ReportsController < ApplicationController
   def index
     app_params_trimmed = app_params.delete_if {|k, v| v.empty?}
     app_filter = { applicants: app_params_trimmed } unless app_params_trimmed.empty?
-    pos_params_trimmed = pos_params.delete_if {|k, v| v.empty?}
+    pos_params_trimmed = pos_params_clean
     pos_filter = { positions: pos_params_trimmed } unless pos_params_trimmed.empty?
     @applicants = Applicant.where("name like?", "%#{app_name_param}%").where(app_filter).joins(:positions).where(pos_filter).distinct.page(params[:page]).order(sort_column + ' ' + sort_direction)
   end
@@ -35,5 +35,21 @@ class ReportsController < ApplicationController
 
     def pos_params
       params.permit(referred: [], application_status: [], course_id: [])
+    end
+
+    def pos_params_clean
+      pos_params.delete_if do |k, v|
+        true if v.empty?
+        array_blank?(v)
+      end
+    end
+
+    def array_blank?(array)
+      return false unless array.kind_of?(Array)
+      array.each do |x|
+        return false unless x.blank?
+      end
+
+      true
     end
 end
